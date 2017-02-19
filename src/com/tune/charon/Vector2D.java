33919bfbe4214e7e7016611de47b86493ff7c358
Tune <mikael.thune@gmail.com>
2017-02-19 10:59:14 +0100

    Added class for 2D vectors

---
+++ src/com/tune/charon/Vector2D.java
@@ -0,0 +1,15 @@
+package com.tune.charon;
+
+
+import javafx.geometry.Point2D;
+
+/**
+ * Created by Tune on 2017-02-19.
+ */
+public class Vector2D extends Point2D
+{
+    public Vector2D(double x, double y)
+    {
+        super(x,y);
+    }
+}
