5fee55f2cfc40c6de9cca6fe3f3a4cb7cd330cfa
Tune <mikael.thune@gmail.com>
2017-02-23 18:34:46 +0100

    Added interface Movable and enum NumericMethod

---
+++ src/com/tune/charon/Movable.java
@@ -0,0 +1,9 @@
+package com.tune.charon;
+
+/**
+ * Created by Tune on 2017-02-23.
+ */
+public interface Movable
+{
+    void applyForce(Vector2D force, double time);
+}
---
+++ src/com/tune/charon/NumericMethod.java
@@ -0,0 +1,9 @@
+package com.tune.charon;
+
+/**
+ * Created by Tune on 2017-02-23.
+ */
+public enum NumericMethod
+{
+    EULER_FORWARD;
+}
