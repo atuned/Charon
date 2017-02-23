c9a259979f3cb4a26ef95379c0948d7578f4d792
Tune <mikael.thune@gmail.com>
2017-02-23 12:09:05 +0100

    New classes

---
+++ src/com/tune/charon/PhysicalObject.java
@@ -0,0 +1,14 @@
+package com.tune.charon;
+
+import javafx.geometry.Point2D;
+
+/**
+ * Created by Tune on 2017-02-19.
+ */
+public interface PhysicalObject
+{
+    private final double mass;
+    private Vector2D velocity;
+    private Point2D position;
+
+}
