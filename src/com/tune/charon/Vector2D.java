8165b24e2f8fe3eea29caf936645555124fe5f4f
Tune <mikael.thune@gmail.com>
2017-02-23 18:42:58 +0100

    Added method to scale and add Vectors

--- src/com/tune/charon/Vector2D.java
+++ src/com/tune/charon/Vector2D.java
@@ -11,4 +11,23 @@ public class Vector2D extends Point2D
     {
         super(x,y);
     }
+
+    public Vector2D(Point2D point2D)
+    {
+        super(point2D.getX(), point2D.getY());
+    }
+
+    public static Vector2D scale(Vector2D vector, double constant)
+    {
+        return new Vector2D(constant * vector.getX(), constant * vector.getY());
+    }
+
+    public static Vector2D add(Vector2D vector1, Vector2D vector2)
+    {
+        return new Vector2D(vector1.add(vector2));
+    }
+
+
+
+
 }
