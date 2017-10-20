8504f9b89ad7117a3a4474f0e58f94926a559086
mithu1 <mikael.thune@gmail.com>
2017-10-20 10:42:45 +0200

    Created Controller class

--- src/com/tune/charon/Main.java
+++ src/com/tune/charon/Main.java
@@ -17,10 +17,6 @@ import java.util.List;
  */
 public class Main extends Application
 {
-    private final int NUM_BALLS = 100;
-    private final int SCENE_HEIGHT = 1000;
-    private final int SCENE_WIDTH = 1800;
-
     public static void main(String[] args)
     {
         launch(args);
@@ -29,46 +25,13 @@ public class Main extends Application
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        List<PhysicalObject> objectList = createObjects();
+        Controller controller = new Controller(200, 1000, 1800, NumericMethod.EULER_FORWARD, 0.2, -9.82);
 
-        PhysicsEngine physicsEngine = new PhysicsEngine(NumericMethod.EULER_FORWARD, 0.2, -9.82);
-        Renderer renderer = new Renderer(physicsEngine);
-
-        List<Node> rendable = new ArrayList<Node>();
-        for (PhysicalObject object : objectList)
-        {
-            if (object instanceof Movable)
-            {
-                physicsEngine.addObjects((Movable) object);
-            }
-
-            if (object instanceof Rendable)
-            {
-                rendable.add(((Rendable) object).getRendition());
-            }
-        }
-
-        Scene scene = new Scene(new Group(rendable), SCENE_WIDTH, SCENE_HEIGHT, Color.BLACK);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(controller.getScene());
         primaryStage.show();
-
-        renderer.start();
+        controller.start();
     }
 
-    private List<PhysicalObject> createObjects()
-    {
-        List<PhysicalObject> objects = new ArrayList<>();
 
-        for (int i = 0; i < NUM_BALLS; i++)
-        {
-            double radius = 10;
-            Vector2D velocity = new Vector2D(Math.random() * 10, Math.random() * 10);
-            Vector2D position = new Vector2D(Math.random() * 0.90 * SCENE_WIDTH + radius,
-                    Math.random() * 0.90 * SCENE_HEIGHT + radius);
-            Ball ball = new Ball(10, 1, radius, velocity, position);
-            objects.add(ball);
-        }
-        return objects;
-    }
 }
 
